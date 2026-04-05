dcab4e5827681ba1f92fb09d962aa421f272656f
Regan Tosh <toshregan94@gmail.com>
2026-04-05 22:29:26 +0300

    New changes

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
+function createLoginTracker(userInfo){
+  let attemptCount =0;
+  return (passwordAttempt)=>{
+    attemptCount++;
+    if (passwordAttempt=== userInfo && attemptCount <=3) {
+      return "Login succesful";
+
+    } else if (attemptCount <=3) {
+      return `Attempt ${attemptCount} : Login failed`;
+    } else{
+      return "Account locked due to too many failed login attempts";
+
+    }
+  }
+}
 
 
-
-module.exports = {
-  ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
-};
